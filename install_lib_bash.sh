9bcb4e25e8b33d0e358e9b612d8f7f750464e555
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-06 20:41:56 +0200

    initial commit

--- install_lib_bash.sh
+++ install_lib_bash.sh
@@ -3,8 +3,15 @@
 function install_lib_bash_if_not_exist {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
     if [[ ! -d "${my_dir}/lib_bash" ]]; then
-        git clone https://github.com/bitranox/lib_bash.git ${my_dir}/lib_bash # > /dev/null 2>&1
+        echo "*******************************************************************************"
+        echo "* installing lib_bash into ${my_dir}/lib_bash"
+        echo "*******************************************************************************"
+        git clone https://github.com/bitranox/lib_bash.git ${my_dir}/lib_bash
         chmod -R +x ${my_dir}/lib_bash/*.sh
+        echo "*******************************************************************************"
+        echo "content : "
+        echo "*******************************************************************************"
+        ls -l
     fi
 }
 
