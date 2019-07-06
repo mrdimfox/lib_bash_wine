9d922e2d4687dcff69841881c50a1d7b9e324cfd
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-06 20:16:34 +0200

    initial commit

--- install_lib_bash.sh
+++ install_lib_bash.sh
@@ -3,7 +3,7 @@
 function install_lib_bash_if_not_exist {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
     if [[ ! -d "${my_dir}/lib_bash" ]]; then
-        git clone https://github.com/bitranox/lib_bash.git ${my_dir}/lib_bash > /dev/null 2>&1
+        git clone https://github.com/bitranox/lib_bash.git ${my_dir}/lib_bash # > /dev/null 2>&1
         sudo chmod -R +x ${my_dir}/lib_bash/*.sh
     fi
 }
@@ -14,9 +14,9 @@ function update_lib_bash_if_exist {
         cd lib_bash
         local git_remote_hash=$(git --no-pager ls-remote --quiet | grep HEAD | awk '{print $1;}' )
         local git_local_hash=$(git --no-pager log --decorate=short --pretty=oneline -n1 | grep HEAD | awk '{print $1;}' )
-        sudo git fetch --all > /dev/null 2>&1
-        git reset --hard origin/master > /dev/null 2>&1
-        sudo chmod -R +x ./*.sh > /dev/null 2>&1
+        sudo git fetch --all # > /dev/null 2>&1
+        git reset --hard origin/master # > /dev/null 2>&1
+        sudo chmod -R +x ./*.sh # > /dev/null 2>&1
         cd ..
     fi
 }
