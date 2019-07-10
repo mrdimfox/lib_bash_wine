15034688455c66696e283f561d3bcff4031368cc
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-10 17:56:38 +0200

    initial commit

--- 003_001_install_wine_python3_preinstalled.sh
+++ 003_001_install_wine_python3_preinstalled.sh
@@ -8,5 +8,6 @@ function include_dependencies {
     source /usr/lib/lib_bash_wine/003_900_lib_install_wine_python_preinstalled.sh
 }
 
+include_dependencies
 update_myself ${0} ${@}                                                              # pass own script name and parameters
 install_wine_python_preinstalled "python37" "Python 3.7" "c:/Python37"
