bad22f2e99447f4457d2578f3f418b9fbf3a4a7a
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-10 18:18:53 +0200

    initial commit

--- 005_001_install_wine_powershell_core.sh
+++ 005_001_install_wine_powershell_core.sh
@@ -37,10 +37,10 @@ function install_powershell_core {
         if [[ "${wine_arch}" == "win32" ]]
             then
                 clr_green "Download Powershell ${powershell_version} 32 Bit"
-                sudo wget -nc --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-${powershell_version}-win-x86.zip
+                sudo wget -nc --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v${powershell_version}/PowerShell-${powershell_version}-win-x86.zip
             else
                 clr_green "Download Powershell ${powershell_version} 64 Bit"
-                sudo wget -nc --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/PowerShell-${powershell_version}-win-x64.zip
+                sudo wget -nc --no-check-certificate -O powershell.zip https://github.com/PowerShell/PowerShell/releases/download/v${powershell_version}/PowerShell-${powershell_version}-win-x64.zip
             fi
 
         unzip -qq ./powershell.zip -d ${powershell_install_dir}
