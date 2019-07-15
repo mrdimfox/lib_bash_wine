a0638cbac72b9a7305d7ed7bee4f459a92f8d5fc
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-16 01:08:51 +0200

    refractor

--- install_or_update.sh
+++ install_or_update.sh
@@ -114,8 +114,3 @@ if [[ $(is_lib_bash_wine_installed) == "True" ]]; then
 else
     install_lib_bash_wine
 fi
-
-
-## make it possible to call functions without source include
-call_function_from_commandline "${0}" "${@}"
-
