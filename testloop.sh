b864ad823d430c26c7939f546223edd3820d0786
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-16 01:24:54 +0200

    refractor

--- testloop.sh
+++ testloop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function test_loop {
+function test_loop_old {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
     local files_to_test=( )
     local actual_file_to_test=""
@@ -14,5 +14,23 @@ function test_loop {
         done
     done
 }
+
+function test_loop {
+    local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
+    local files_to_test=( )
+    local actual_file_to_test=""
+
+
+    while [[ 1=1 ]]; do
+        files_to_test=( $(sudo ls "${my_dir}"/*.sh ) )
+        for actual_file_to_test in "${files_to_test[@]}"
+        do
+            source ${actual_file_to_test}
+            tests
+        done
+    done
+}
+
+
 
 test_loop
