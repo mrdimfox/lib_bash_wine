447af83323707edaf468f24c3d3f8bfd045d093e
bitranox <rnowotny1966@gmail.com>
2019-07-17 20:36:36 +0200

    refractor

--- 006_000_install_wine_chocolatey_non_functional.sh
+++ 006_000_install_wine_chocolatey_non_functional.sh
@@ -4,7 +4,7 @@ export SUDO_ASKPASS="$(command -v ssh-askpass)"
 export NO_AT_BRIDGE=1  # get rid of (ssh-askpass:25930): dbind-WARNING **: 18:46:12.019: Couldn't register with accessibility bus: Did not receive a reply.
 
 export bitranox_debug_global="${bitranox_debug_global}"  # set to True for global Debug
-# export debug_lib_bash_wine="False"
+export debug_lib_bash_wine="${debug_lib_bash_wine}"  # set to True for Debug in lib_bash_wine
 
 
 function update_myself {
