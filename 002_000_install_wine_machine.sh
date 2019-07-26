8d0762d538cd90f32e12229f364ff539e999186f
bitranox <rnowotny1966@gmail.com>
2019-07-27 01:20:52 +0200

    refractor

--- 002_000_install_wine_machine.sh
+++ 002_000_install_wine_machine.sh
@@ -82,9 +82,9 @@ function install_wine_machine {
     fix_wine_permissions "${wine_prefix}" "${user}" # it is cheap, just in case
     wait_for_file_to_be_created "${wine_prefix}"/system.reg
 
-    banner "Installing wine mono on ${wine_prefix}"
-    install_wine_mono "${wine_prefix}" "${user}"
-    fix_wine_permissions "${wine_prefix}" "${user}" # it is cheap, just in case
+    # banner "Installing wine mono on ${wine_prefix}"
+    # install_wine_mono "${wine_prefix}" "${user}"
+    # fix_wine_permissions "${wine_prefix}" "${user}" # it is cheap, just in case
 
     banner "Installing wine gecko on ${wine_prefix}"
     install_wine_gecko "${wine_prefix}" "${user}"
